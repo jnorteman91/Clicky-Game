fdadbb1491d2946407d48540173293622d16e18e
Jd0h Norteman <jnorteman91@gmail.com>
2018-11-21 16:58:57 -0500

    added containers and columns

--- clickygame/src/Column.js
+++ clickygame/src/Column.js
@@ -1,1 +1,9 @@
 import React from "react";
+const Column = props => {
+    const size = props.size.split(" ").map(size => "col-" + size).join(" ");
+    return (
+        <div className={size}>
+            {props.children}
+        </div>
+    );
+};
