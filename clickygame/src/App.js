b1a6d9bce911cb6cc3f095f26646aab86a22acd2
Jd0h Norteman <jnorteman91@gmail.com>
2018-11-21 21:29:09 -0500

    adjusted nav and css

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-function shuffle(array) {
+function shuffleFriends(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
