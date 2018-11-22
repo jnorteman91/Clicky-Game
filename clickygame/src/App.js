d2c88cbf155c0d3a4eaa71877f3f42fc9c1276f2
Jd0h Norteman <jnorteman91@gmail.com>
2018-11-21 21:45:39 -0500

    edited app.js

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,6 +1,13 @@
-import React, { Component } from 'react';
-
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
+import friends from "./friends.json";
+import "./App.css";
 
 function shuffleFriends(array) {
   for (let i = array.length - 1; i > 0; i--) {
